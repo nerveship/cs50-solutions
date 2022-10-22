6f1be3bb12b11782fee5148415b8e83559b27c29
John-Stuart Gonsales <johnstuartgonsales@gmail.com>
2022-10-22 12:05:34 +0100

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+   int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+  for (int row = 0; row < height; row++)
+  {
+    for (int space = 0; space < (height - row) - 1; space++)
+    {
+      printf(" ");
+    }
+    for (int hash = 0; hash < 1 + row; hash++)
+    {
+      printf("#");
+    }
+   printf("\n");
+  }
+}
